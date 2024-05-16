2a2034109efab4268fdec2284a9f670a12f7e5cf
Jafagervik <jorgenafagervik@gmail.com>
2024-05-16 19:31:27 +0200

    slkjf

--- test.jl
+++ test.jl
@@ -3,6 +3,7 @@ using Dates
 using LinearAlgebra
 using Plots
 
+# 75 000 , 2137
 function run()
     h5open("./data/FORESEE_UTC_20200301_000015.hdf5", "r") do f
 
@@ -10,14 +11,15 @@ function run()
         @show typeof(data)
         @show size(data)
 
-        @show data[1]
-
         timestamp = f["timestamp"][]
-        @show typeof(timestamp)
 
         #print(datetime.fromtimestamp(timestamp[0]))
 
-        p = heatmap(data, color=:viridis)
+        p = heatmap(1:size(data, 1),
+            1:size(data, 2), data,
+            c=:viridis,
+            xlabel="Channels", ylabel="Time",
+            title="YEET")
         savefig(p, "./figs/heatmap.png")
 
     end
